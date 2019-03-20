544ed591faf297595c3e4bfd4698ad2ec9e8d8be
svrakata <svrakata@gmail.com>
2019-03-20 23:00:52 +0200

    11

--- script.js
+++ script.js
@@ -14,38 +14,56 @@ let r;
 let factor = 0;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  r = height / 2 - 16;
+    if (windowWidth < 700) {
+        createCanvas(windowWidth, windowHeight);
+    } else {
+        createCanvas(windowWidth / 2, windowHeight / 2);
+    }
+
+    if (windowWidth < 400) {
+        r = width / 2 - 16;
+    } else {
+        r = height / 2 - 16;
+    }
 }
 
 function getVector(index, total) {
-  const angle = map(index % total, 0, total, 0, TWO_PI);
-  const v = p5.Vector.fromAngle(angle + PI);
-  v.mult(r);
-  return v;
+    const angle = map(index % total, 0, total, 0, TWO_PI);
+    const v = p5.Vector.fromAngle(angle + PI);
+    v.mult(r);
+    return v;
 }
 
 function draw() {
-  background(0);
-  const total = 200; //int(map(mouseX, 0, width, 0, 200));
-  factor += 0.015;
+    background(0);
+    const total = 200; //int(map(mouseX, 0, width, 0, 200));
+    factor += 0.0005;
 
-  translate(width / 2, height / 2);
-  stroke(255, 150);
-  strokeWeight(2);
-  noFill();
-  ellipse(0, 0, r * 2);
+    translate(width / 2, height / 2);
+    stroke(255, 150);
+    strokeWeight(2);
+    noFill();
+    ellipse(0, 0, r * 2);
 
-  strokeWeight(2);
-  for (let i = 0; i < total; i++) {
-    const a = getVector(i, total);
-    const b = getVector(i * factor, total);
-    line(a.x, a.y, b.x, b.y);
-  }
+    strokeWeight(2);
+    for (let i = 0; i < total; i++) {
+        const a = getVector(i, total);
+        const b = getVector(i * factor, total);
+        line(a.x, a.y, b.x, b.y);
+    }
 }
 
+window.addEventListener('resize', function () {
 
-window.addEventListener('resize', function() {
-    resizeCanvas(window.innerWidth, window.innerHeight);
+    if (windowWidth < 700) {
+        resizeCanvas(windowWidth, windowHeight);
+    } else {
+        resizeCanvas(windowWidth / 2, windowHeight / 2);
+    }
+
+    if (windowWidth < 400) {
+        r = width / 4 - 16;
+    }
+
     translate(width / 2, height / 2);
 });
